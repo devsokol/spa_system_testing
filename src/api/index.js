91aabab67dc497cf979a9526de4dc6856b85a8de
Andriy <devsokol@ukr.net>
2019-11-13 22:04:31 +0200

    two test commit

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // import store from '@/store'
 import config from '@/default_config'
 
-axios.defaults.baseURL = config.serverName2
+axios.defaults.baseURL = config.serverName
 
 // store.getters['user/getUsers'] - example get getters
 
